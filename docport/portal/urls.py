9a0aebe6baf10e9c264e3bd5541fdb381a050ec8
Vitaliy Turkevich <vetalturkevich@gmail.com>
2025-06-10 15:11:15 -0400

    Add article models and CRUD views

--- docport/portal/urls.py
+++ docport/portal/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('articles/', views.article_list, name='article_list'),
+    path('articles/create/', views.article_create, name='article_create'),
+    path('articles/<int:pk>/edit/', views.article_edit, name='article_edit'),
 ]
--- docport/portal/views.py
+++ docport/portal/views.py
@@ -1,5 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+
+from .models import Article
+from .forms import ArticleForm
 
 
 def index(request):
     return render(request, 'portal/index.html')
+
+
+def article_list(request):
+    articles = Article.objects.order_by('-published_at')
+    return render(request, 'portal/article_list.html', {'articles': articles})
+
+
+@login_required
+def article_create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.save()
+            return redirect('article_list')
+    else:
+        form = ArticleForm()
+    return render(request, 'portal/article_form.html', {'form': form})
+
+
+@login_required
+def article_edit(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('article_list')
+    else:
+        form = ArticleForm(instance=article)
+    return render(request, 'portal/article_form.html', {'form': form, 'article': article})
